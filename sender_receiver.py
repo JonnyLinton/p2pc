ada1139cfb82b4f8d563dbb5cd805f5006116289
Louis-Olivier Guérin <sites+github@louisolivier.com>
2017-03-24 18:16:50 -0400

    Fixed tuple problems

--- sender_receiver.py
+++ sender_receiver.py
@@ -1,17 +1,19 @@
 import socket
 from parsing import build_message, parse_message
+from display import display
 
 def sender(user_name, ip_address, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     while True:
         user_message = input()
         application_message = build_message(user_message, user_name)
-        sock.sendto(application_message, ip_address, port)
+        sock.sendto(application_message.encode("utf-8"), (ip_address, port))
 
 def receiver(port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind("", port)
+    sock.bind(("", port))
     while True:
         application_message = sock.recv(1024)
-        (user_name, user_message) = parse_message(application_message)
-        print(user_name, user_message)
+        message = application_message.decode("utf-8")
+        (user_name, user_message) = parse_message(message)
+        display(user_name, user_message)
