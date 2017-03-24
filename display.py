43c56f5de6b64302b27b1169ee048c4f7a76bcae
Louis-Olivier Guérin <sites+github@louisolivier.com>
2017-03-24 18:43:58 -0400

    Better formatting

--- display.py
+++ display.py
@@ -2,4 +2,4 @@ from datetime import datetime
 
 def display(user_name, user_message):
     current_time = datetime.now()
-    print(str(current_time) + " " + user_name + ": " + user_message)
+    print(str(current_time) + " " + "<" + user_name + ">: " + user_message)
