42e94c4b8799deab1617aaec55a31fbbd830bd7b
Louis-Olivier Guérin <sites+github@louisolivier.com>
2017-03-24 13:24:00 -0400

    build_message and parse_message

---
+++ parsing.py
@@ -0,0 +1,14 @@
+import re
+
+def build_message(user_message, user_name):
+    return "user: " + user_name + "\n" + "message: " + user_message + "\n\n"
+
+def parse_message(application_message):
+    dict = {}
+    lines =  re.split("\n+", application_message)
+    message_list = [re.split(": ", entry, 2) for entry in lines]
+    for elements in message_list:
+        if len(elements) > 1:
+            dict[elements[0]] = elements[1]
+    return (dict["user"], dict["message"])
+    
