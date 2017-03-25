b357d8e31303f389251a43a6438ec30e4a6caf41
Louis-Olivier Guérin <sites+github@louisolivier.com>
2017-03-24 22:33:57 -0400

    Added temporary global variables

--- p2pc.py
+++ p2pc.py
@@ -1,9 +1,9 @@
 import threading
 from sender_receiver import sender, receiver
+BROADCAST_IP = "255.255.255.255"
+PORT = 8080
 
 def run():
     user_name = input("Enter your name: ")
-    port = 8080
-    ip = "255.255.255.255"
-    threading.Thread(target=sender, args=(user_name, ip, port)).start()
+    threading.Thread(target=sender, args=(user_name, BROADCAST_IP, PORT)).start()
     threading.Thread(target=receiver, args=(port,)).start()
