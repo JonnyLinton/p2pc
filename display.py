84eb01494909c43a3803a10b316d6c236d39a43f
JonnyLinton <linton.jonny@gmail.com>
2017-03-30 19:33:18 -0400

    fixing something I accidentally removed.

--- display.py
+++ display.py
@@ -1,10 +1,15 @@
 from datetime import datetime
 
+from commands import command_quit
+
+
 def display(parsed_message):
     user_name, command, message = parsed_message["user"], parsed_message["command"], parsed_message["message"]
     current_time = datetime.now()
 
-    if command in ["/leave", "/join"]:
+    if command == "/quit":
+        command_quit()
+    elif command in ["/leave", "/join"]:
         print(str(current_time) + " " + user_name + " " + message)
     else:
         print(str(current_time) + " " + "<" + user_name + ">: " + message)
