533ea1c6224a48a17bfdd5748631b995548e915b
JonnyLinton <linton.jonny@gmail.com>
2017-04-16 21:57:10 -0400

    Merging channels into master

--- parsing.py
+++ parsing.py
@@ -1,6 +1,6 @@
 import re
 from current_channel import current_channel
-from channels import channels
+
 
 def build_message(user_name, ip, channel, command, message):
     return "user: " + user_name + "\n" + "ip: " + ip + "\n" + "channel: " + channel + "\n" + "command: " + command + "\n" + "message: " + message + "\n\n"
@@ -35,21 +35,3 @@ def parse_message(application_message):
         if len(elements) > 1:
             message_params[elements[0]] = elements[1]
     return message_params
-
-def is_unique(user_name):
-    for name, ip in channels[current_channel[0]]:
-        if user_name == name:
-            return False
-    return True
-
-
-def make_user_name_unique(user_name, unique_id = 0):
-    if is_unique(user_name):
-        return user_name
-    else:
-        unique_id = unique_id + 1
-        new_user_name = user_name
-        if "-" in new_user_name:
-            new_user_name = user_name.split("-")[0]
-        new_user_name = new_user_name + "-" + str(unique_id)
-        return make_user_name_unique(new_user_name, unique_id)
